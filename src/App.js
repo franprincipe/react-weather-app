196c6acb9b46ee12dfa4066ee045b19922dfa477
Francisca Principe <franciscaprincipe0@gmail.com>
2023-10-17 19:33:42 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.css';
 export default function App() {
   return (
     <div className="App">
-      <div className='container'>
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project was coded by Francisca Principe{""}
