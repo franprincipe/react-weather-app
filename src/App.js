4f6e7b0e288b3ca9a4cc72e5f4004723ce299b46
Francisca Principe <franciscaprincipe0@gmail.com>
2023-10-18 19:55:49 -0400

    Started the structure of Weather App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Weather from "./Weather";
 
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 export default function App() {
@@ -11,7 +12,7 @@ export default function App() {
       <footer>
         This project was coded by Francisca Principe{""}
      {""} and is {""}
-        <a href="https://github.com/franprincipe/react-weather-app" target="_blank">
+        <a href="https://github.com/franprincipe/react-weather-app" alt="Francisca" target="_blank">
           open-sourced 
         </a> on GitHub.
        </footer>
