9f3c3ee30a4f97b27b8d1580632174bc49bbd73d
Francisca Principe <franciscaprincipe0@gmail.com>
2023-10-20 21:17:37 -0400

    removed unnecessary files

--- src/Weather.js
+++ src/Weather.js
@@ -5,12 +5,11 @@ import "./Weather.css";
 export default function Weather(props) {
     const [weatherData, setWeatherData] = useState({ready: false});
     function handleResponse(response) {
-        console.log(response.data);
         setWeatherData({
             ready: true,
             temperature: response.data.main.temp,
             humidity: response.data.main.humidity,
-            date:"Wednesday 07:00",
+            date:new Date(response.data.dt * 1000),
             description: response.data.weather[0].description,
             iconUrl:"https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
             wind: response.data.wind.speed,
@@ -26,7 +25,7 @@ export default function Weather(props) {
             <div className="col-9">
             <input 
                 type="search" 
-                placeholder="Enter a city"
+                placeholder="Search city"
                 className="form-control"/>
             </div>
             <div className="col-3">
@@ -39,7 +38,7 @@ export default function Weather(props) {
         </form>
             <h1>{weatherData.city}</h1>
             <ul>
-                <li>{weatherData.date}</li>
+                <li>{weatherData.date.getDay()}</li>
                 <li className="text-capitalize">{weatherData.description}</li>
             </ul>
     <div className="row mt-3">
