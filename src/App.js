696cda882b49b8b38117456117607ee002ec17a0
Francisca Principe <franciscaprincipe0@gmail.com>
2023-11-22 14:08:52 -0500

    Added API integration

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <Weather />
+      <Weather defaultCity="Willow Spring"/>
       <footer>
         This project was coded by Francisca Principe{""}
      {""} and is {""}
--- src/Weather.js
+++ src/Weather.js
@@ -1,42 +1,67 @@
-import React from "react"; 
+import React, { useState } from "react"; 
+import axios from "axios";
 import "./Weather.css";
-export default function Weather() {
-    return <div className="Weather">
-        <form>
-        <div className="row">
-        <div className="col-9">
-            <input 
-            type="search"
-            placeholder="Search for City..."
-            className="form-control"
-            autoFocus="on"
-            />
-            </div>
-            <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary w-100" 
-            />
-            </div>
-        </div>
-        </form> 
-        <h1>Willow Spring</h1>
-        <ul>
-            <li>Sunday, 01:37</li>
-            <li>Mostly Sunny</li>
-        </ul>
-        <div className="row mt-3">
-            <div className="col-6">
-                
-                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly Sunny"/>  
-                <span className="temperature">16</span>
-                <span className="unit">°C</span>
-            </div>
+
+export default function Weather(props) {
+    const [weatherData, setWeatherData] = useState({ready: false});
+
+    function handleResponse(response) {
+        setWeatherData({
+            ready: true,
+        temperature:response.data.main.temp,
+        humidity: response.data.main.humidity,
+        date: "Sunday, 01:37",
+        description: response.data.weather[0].description,
+        iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+        wind: response.data.wind.speed,
+        city: response.data.name
+    });
+    }
+
+    if (weatherData.ready) {
+        return (
+            <div className="Weather">
+                 <form>
+                 <div className="row">
+                 <div className="col-9">
+                     <input 
+                     type="search"
+                     placeholder="Search for City..."
+                     className="form-control"
+                     />
+                     </div>
+                     <div className="col-3">
+                     <input type="submit" value="Search" className="btn btn-primary w-100" 
+                     />
+                     </div>
+                 </div>
+                 </form> 
+                 <h1>{weatherData.city}</h1>
+                 <ul>
+                     <li>{weatherData.date}</li>
+                     <li className="text-capitalize">{weatherData.description}</li>
+                 </ul>
+                 <div className="row mt-3">
+                     <div className="col-6">
+                         
+                         <img src={weatherData.iconUrl} alt={weatherData.description} />  
+                         <span className="temperature">{Math.round(weatherData.temperature)}</span>
+                         <span className="unit">°C</span>
+                     </div>
         <div className="col-6">
             <ul>
-                <li>Precipitation: 0%</li>
-                <li>Humidity: 42%</li>
-                <li>Wind: 19 km/h</li>     
+                <li>Humidity: {weatherData.humidity} %</li>
+                <li>Wind: {weatherData.wind} km/h</li>     
             </ul>
-        </div>
-        </div>
-    </div> 
+             </div>
+          </div>
+        </div> 
+      );
+    } else {
+    const apiKey = "ca0db41e2e878c74a1dfc7ffece370d4";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+    }
 }
