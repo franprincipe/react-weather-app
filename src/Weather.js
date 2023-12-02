64ddb1211d9065e8c802e47d5c33d935e9b0b908
Francisca Principe <franciscaprincipe0@gmail.com>
2023-12-02 11:37:45 -0500

    Added Weather Forecast structure

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"; 
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
@@ -57,7 +58,8 @@ export default function Weather(props) {
                  </div>
                  </div>
                  </form> 
-                 <WeatherInfo data={weatherData} />         
+                 <WeatherInfo data={weatherData} /> 
+                 <WeatherForecast />        
                 </div> 
       );
     } else {
