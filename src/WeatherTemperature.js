250cdeb67185e00f73ae63646b35b18fc73cc10e
Francisca Principe <franciscaprincipe0@gmail.com>
2023-12-12 11:14:27 -0500

    Removed celsius temperature from app

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,47 +1,12 @@
 import React, { useState } from "react";
 
-export default function WeatherTemperature(props) {
-    const [unit, setUnit] = useState("fahrenheit");
-
-    function showCelsius(event) {
-        event.preventDefault();
-        setUnit("celsius");
-    }
-    
-    function showFahrenheit(event) {
-        event.preventDefault();
-        setUnit("fahrenheit");
-    }
-
-    function celsius() {
-        return ((props.fahrenheit - 32) * 5) / 9;
-    }
-
-    if (unit === "fahrenheit") { 
-        return (
-        <div className="WeatherTemperature">
-            <span className="temperature">{Math.round(props.fahrenheit)}</span>
-            <span className="unit">  
-            °F      | {" "}
-
-                <a className="text-decoration-none" href="/" onClick={showCelsius}>
-                °C
-                </a> 
-             
-            </span>
-        </div>
-        );
-} else {
+export default function WeatherTemperature(props) { 
     return (
     <div className="WeatherTemperature">
-    <span className="temperature">{Math.round(celsius())}</span>
+    <span className="temperature">{Math.round(props.fahrenheit)}</span>
     <span className="unit">
-     °C | {" "}
-    <a className="text-decoration-none" href="/" onClick={showFahrenheit}>
      °F
-    </a>
 </span>
 </div>
 );
     }
-}
